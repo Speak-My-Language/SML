8a71b557052ebc494f4babb6e637bcf2bb3e2a06
eric <ericzfwu@gmail.com>
2022-04-13 10:54:38 -0400

    Merge Dev

--- __tests__/components/ChartContainerTests.js
+++ __tests__/components/ChartContainerTests.js
@@ -2,7 +2,7 @@ import React from 'React';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import ChartContainer from '../client/components/ChartContainer.jsx';
+import ChartContainer from '../../client/components/ChartContainer.jsx';
 
 describe('Testing the ChartContainer component', () => {
   const props = {
