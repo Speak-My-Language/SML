2f21e5e0997e954f13c0e55fdbd6305af3bc5296
StanislausA <anthonystanislaus1@gmail.com>
2022-04-13 12:04:05 -0400

    before merge

--- __tests__/components/MatchContainerTest.js
+++ __tests__/components/MatchContainerTest.js
@@ -1,12 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+// Above docblock is required in order to run tests in browser-like environment
+
+import { render } from '@testing-library/react';
 import React from 'react';
 import MatchContainer from '../../client/components/MatchContainer';
 
-// Skipped for time being
+// Requires some more tests, specifically, what is our plan for this page?
 describe('Unit testing MatchesContainer component', () => {
   let component;
   beforeAll(() => component = render(<MatchContainer />));
 
-  xtest('::PLACEHOLDER::', () => {
-    return false;
+  test('Matches container renders on screen', () => {
+    const container = component.getByTestId('matches-container');
+    expect(container.toBeInTheDocument());
   });
 });
