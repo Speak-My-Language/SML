927980d982134e24376b79be49210a806e17a7fe
StanislausA <anthonystanislaus1@gmail.com>
2022-04-13 12:37:38 -0400

    logoutController progress blocked

---
+++ __tests__/controllers/logoutControllerTest.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+// Above docblock is required in order to run tests in browser-like environment
+
+// Skipped for time being, logout functionality changes url to root endpoint
+xdescribe('Unit testing logoutController', () => {
+
+  test('::PLACEHOLDER::', () => {
+    return false;
+  });
+});
--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -2,8 +2,8 @@ const db = require('../models/userModel.js');
 
 const logoutController = {};
 
-// logoutController.methodThatWeDefine = (req, res, next) => {
-
-// }
+logoutController.logout = (req, res, next) => {
+  
+}
 
 module.exports = logoutController;
