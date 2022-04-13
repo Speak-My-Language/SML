ff974d514a6d87c74096f10e5a203022c84ef26f
StanislausA <anthonystanislaus1@gmail.com>
2022-04-12 22:26:03 -0400

    attempted test on signupContainer url change

--- __tests__/components/SignupContainerTest.js
+++ __tests__/components/SignupContainerTest.js
@@ -3,19 +3,25 @@
  */
 // Above docblock is required in order to run tests in browser-like environment
 
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import SignupContainer from "../../client/components/SignupContainer";
 
 
 describe('Unit testing signupContainer component', () => {
   let component;
+  beforeAll(() => component = render(<SignupContainer />));
 
-  beforeAll(() => {
-    component = render(<SignupContainer />);
-  });
-
-  test('Button renders with text "sign in with github"', () => {
+  xtest('Button renders with text "sign in with github"', () => {
     expect(component.getAllByText('sign in with github').length).toEqual(1);
   });
+
+  // test('Button onClick event changes url to "/github/auth"', async () => {
+  //   const user = userEvent.setup();
+  //   const button = component.getByRole('button');
+  //   console.log(global.window.location.pathname);
+  //   await user.click(button);
+  //   console.log(global.window.location.pathname);
+  // });
 });
