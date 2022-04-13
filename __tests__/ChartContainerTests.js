b362e3bb3c69949f9f8b46f8cd9f56558ea3a0f4
eric <ericzfwu@gmail.com>
2022-04-13 10:51:21 -0400

    Merge with dev

---
+++ __tests__/ChartContainerTests.js
@@ -0,0 +1,11 @@
+import React from 'React';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import ChartContainer from '../client/components/ChartContainer.jsx';
+
+describe('Testing the ChartContainer component', () => {
+  const props = {
+    languages: '{"HTML":5000,"CSS":4000,"JavaScript":1000000}',
+  };
+});
